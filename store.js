4e359f705d42a7eaa1cf98348936b4c6a88531b7
TomaszMatyla <tomek_matyla@tlen.pl>
2020-03-28 09:33:58 +0000

    Dodano wyświetlanie listy zadań

--- store.js
+++ store.js
@@ -34,3 +34,23 @@ module.exports = {
   init,
   createTask
 }
+
+const listTasks = async () => (
+  new Promise((resolve, reject) => {
+    const query = new storage.TableQuery()
+      .select(['title'])
+      .where('PartitionKey eq ?', 'task')
+
+    service.queryEntities(table, query, null, (error, result, response) => {
+      !error ? resolve(result.entries.map((entry) => ({
+        title: entry.title._
+      }))) : reject()
+    })
+  })
+)
+
+module.exports = {
+  init,
+  createTask,
+  listTasks
+}
