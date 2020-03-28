e5629bc3280a1f6a05f4b4b48af1ab8a885e348d
TomaszMatyla <tomek_matyla@tlen.pl>
2020-03-28 10:17:46 +0000

    Dodano możliwość zmiany statusu zadania

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,6 +3,16 @@ const store = require('../store')
 
 router.prefix('/tasks')
 
+router.post('/updateStatus', async (ctx, next) => {
+  const { id, status } = ctx.request.body
+  await store.updateTaskStatus(id, status)
+  ctx.status = 200
+})
+
+
+
+
+
 router.post('/add', async (ctx, next) => {
   await store.createTask(ctx.request.body.title)
   ctx.redirect('/')
--- store.js
+++ store.js
@@ -21,7 +21,8 @@ const createTask = async (title) => (
     const task = {
       PartitionKey: generator.String('task'),
       RowKey: generator.String(uuid.v4()),
-      title
+      title,
+      status: 'open'
     }
 
     service.insertEntity(table, task, (error, result, response) => {
@@ -38,12 +39,14 @@ module.exports = {
 const listTasks = async () => (
   new Promise((resolve, reject) => {
     const query = new storage.TableQuery()
-      .select(['title'])
+      .select(['RowKey', 'title', 'status'])
       .where('PartitionKey eq ?', 'task')
 
     service.queryEntities(table, query, null, (error, result, response) => {
       !error ? resolve(result.entries.map((entry) => ({
-        title: entry.title._
+        id: entry.RowKey._,
+        title: entry.title._,
+        status: entry.status._
       }))) : reject()
     })
   })
@@ -53,4 +56,25 @@ module.exports = {
   init,
   createTask,
   listTasks
+}
+
+const updateTaskStatus = async (id, status) => (
+  new Promise((resolve, reject) => {
+    const generator = storage.TableUtilities.entityGenerator
+    const task = {
+      PartitionKey: generator.String('task'),
+      RowKey: generator.String(id),
+      status
+    }
+
+    service.mergeEntity(table, task, (error, result, response) => {
+      !error ? resolve() : reject()
+    })
+  })
+)
+module.exports = {
+  init,
+  createTask,
+  listTasks,
+  updateTaskStatus
 }
