c6a7f4a13e42ace79bc904562c53560ef43f2093
TomaszMatyla <tomek_matyla@tlen.pl>
2020-03-28 09:51:29 +0000

    Dodano wyświetlanie listy zadań

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,11 @@
-const store = require('../store')
 const router = require('koa-router')()
+
+const store = require('../store')
 
 router.get('/', async (ctx, next) => {
   tasks = await store.listTasks()
   await ctx.render('index', { tasks })
 })
-
-
-
-
-
-
-//router.get('/', async (ctx, next) => {
-  //await ctx.render('index', {
-    //title: 'Hello Koa 2!'
-  //})
-//})
 
 router.get('/string', async (ctx, next) => {
   ctx.body = 'koa2 string'
